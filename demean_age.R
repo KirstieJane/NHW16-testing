befc7bc4e499c39df82afa76176b1dc6e14e03bf
Kirstie Whitaker <kw401@cam.ac.uk>
2016-09-06 01:55:48 +0100

    unbroke the command

--- demean_age.R
+++ demean_age.R
@@ -4,6 +4,6 @@ demean_age <- age - sum(age)/length(age)
 
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
-echo "I think this will break the code!"
+print("you need to make sure to use R syntax!")
 
 print("done!")
