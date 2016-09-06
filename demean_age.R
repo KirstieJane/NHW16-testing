a8c25cff8074a49a946d1b416e3cc677d171b9c3
Kirstie Whitaker <kw401@cam.ac.uk>
2016-09-06 02:11:08 +0100

    added assertion tests to r script

--- demean_age.R
+++ demean_age.R
@@ -5,6 +5,11 @@ demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 age <- demographics[4]
 demean_age <- age - sum(age)/length(age)
 
+# Add in the assertion tests
+stopifnot(mean_age < 100)
+stopifnot(mean_age > 10)
+
+# Carry on if the tests pass :)
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
