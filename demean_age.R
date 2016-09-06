9048bd17fd7a1cdec4881e924e25143328073ad7
Kirstie Whitaker <kw401@cam.ac.uk>
2016-09-06 02:14:37 +0100

    added in line to actually CALCULATE mean age to R script

--- demean_age.R
+++ demean_age.R
@@ -3,13 +3,17 @@ args <- commandArgs(trailingOnly = TRUE)
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 
 age <- demographics[4]
-demean_age <- age - sum(age)/length(age)
+mean_age <- sum(age)/length(age)
 
 # Add in the assertion tests
+# to check that the mean age is
+# between 100 and 10
 stopifnot(mean_age < 100)
 stopifnot(mean_age > 10)
 
 # Carry on if the tests pass :)
+demean_age <- age - sum(age)/length(age)
+
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
